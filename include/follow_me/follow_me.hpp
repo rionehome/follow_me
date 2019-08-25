53f7aefaa18095274c17404f2cd4fa54105ac0a0
itomasaki <l1sum@ito.local>
2019-08-25 11:55:16 +0900

    ヘッダーファイルをC++のものにした(しなくても良かった)

--- include/follow_me/follow_me.hpp
+++ include/follow_me/follow_me.hpp
@@ -1,45 +1,86 @@
 //
 // Created by migly on 19/07/13.
 //
-#include "ros/ros.h"
-#include "std_msgs/String.h"
-#include "sensor_msgs/Image.h"
-#include <sensor_msgs/LaserScan.h>
-#include <geometry_msgs/Twist.h>
-#include <cv_bridge/cv_bridge.h>
-#include <opencv2/highgui.hpp>
-#include <std_msgs/Float64MultiArray.h>
-#include <math.h>
+// Modification by ItoMasaki on 19/08/24
+//
+
+
+//#include "ros/ros.h"
+//#include "std_msgs/String.h"
+//#include "sensor_msgs/Image.h"
+//#include <sensor_msgs/LaserScan.h>
+//#include <geometry_msgs/Twist.h>
+//#include <cv_bridge/cv_bridge.h>  not to use
+//#include <opencv2/highgui.hpp>    not to use
+//#include <std_msgs/Float64MultiArray.h>
+//#include <math.h>
+//#include <iostream>
+//#include <cmath>
+//#include "move/Velocity.h"
+//#include <nav_msgs/Odometry.h>
+//#include <float.h>
+//
+//#ifndef SRC_FOLLOW_H_H
+//#define SRC_FOLLOW_H_H
+
 #include <iostream>
 #include <cmath>
-#include "move/Velocity.h"
-#include <nav_msgs/Odometry.h>
-#include <float.h>
 
-#ifndef SRC_FOLLOW_H_H
-#define SRC_FOLLOW_H_H
+#include <rclcpp/rclcpp.hpp>
+#include <std_msgs/msg/String.hpp>
+#include <sensor_msgs/msg/Image.hpp>
+#include <sensor_msgs/msg/LaserScan.hpp>
+#include <geometry_msgs/msg/Twist.hpp>
+#include <nav_msgs/msg/Odometry.hpp>
+
 #define MAX_LINEAR 0.7 // m/s
 #define MAX_ANGULAR 1.9 // rad
 
-class Follow
-{
-public:
-    explicit Follow(ros::NodeHandle *n);
-    ~Follow();
+class Follow_me: public rclcpp::Node {
+	private :
+		rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr Velocity_Publisher;
+		rclcpp::Subscriber<sensor_msgs::msg::LaserScan>::SharedPtr Ydlidar_Subscription;
+		rclcpp::Subscription<std_msgs::msg::String>::SharedPtr Signal_Subscription;
+		rclcpp::Subscription<geometry_msgs::msg::Odometry>::SharedPtr Odometry_Subscription;
 
-    //インデックスごとの構造体を作成
-    typedef struct
-    {
-        int index;
-        cv::Point point;
-        double existence_rate;
-    } SampleData;
+		void Ydlidar_Callback(sensor_msgs::msg::LaserScan::SharedPtr msg);
+		void Odometry_Callback(nav_msgs::msg::Odometry::SharedPtr msg);
+		void Signal_Callback(std_msgs::msg::String::SharedPtr msg);
 
-    ros::Publisher velocity_pub;
-    ros::Subscriber ydlidar_sub;
-    ros::Subscriber signal_sub;
-    ros::Subscriber odom_sub;
-    ros::Publisher twist_pub;
+		rmw_qos_profile_t SENSOR_QOS_PROFILE = rmw_qos_profile_sensor_data;
+		rmw_qos_profile_t PARAMETER_QOS_PROFILE = rmw_qos_profile_parameters;
+
+	public :
+		Follow_me() :
+			Node("follow_me"){
+				Ydlidar_Subscription = this->create_subscription<sensor_msgs::msg::LaserScan>(
+					"scan",
+					[this](sensor_msgs::msg::LaserScan::SharedPtr msg){
+						Ydlidar_Callback(msg);
+					},
+					SENSOR_QOS_PROFILE
+				);
+
+				Signal_Subscription = this->create_subscription<sensor_msgs::msg::LaserScan>(
+					"signal",
+					[this](sensor_msgs::msg::LaserScan::SharedPtr msg){
+						Signal_Callback(msg);
+					},
+					PARAMETER_QOS_PROFILE
+				);
+
+				Odometry_Subscription = this->create_subscription<nav_msgs::msg::Odometry>(
+					"/odom",
+					[this](nav_msgs::msg::Odometry::SharedPtr msg){
+						Odometry_Callback(msg);
+					},
+					SENSOR_QOS_PROFILE
+				)
+			}
+
+}
+
+///////////////////////////////////////////////
 
     std_msgs::Float64MultiArray info;
     std::vector<double> ydlidar_ranges;
@@ -84,10 +125,6 @@ public:
         }
     }
 
-    void ydlidar_callback(const sensor_msgs::LaserScan::ConstPtr &msgs);
-
-    void odom_callback(const boost::shared_ptr<const nav_msgs::Odometry_<std::allocator<void>>> &odom);
-
     void updatePlayerPoint(const sensor_msgs::LaserScan_<std::allocator<void>>::ConstPtr &msgs);
 
     double toQuaternion_degree(double w, double z)
@@ -124,4 +161,4 @@ public:
     }
 };
 
-#endif //SRC_FOLLOW_H_H
+//#endif //SRC_FOLLOW_H_H
