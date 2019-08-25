329e45fccff01a30cf0533a7f6f6afb963494420
l1sum <l1sum@icloud.com>
2019-08-25 12:45:39 +0900

    write kalman filter header file

--- include/ExtendedKalmanFilter/ExtendedKalmanFilter.hpp
+++ include/ExtendedKalmanFilter/ExtendedKalmanFilter.hpp
@@ -1,20 +1,26 @@
 #include <cmath>
-#include <vector>
 
-using namespace std;
+#include </usr/include/eigen3/Eigen/Core>
+#include </usr/include/eigen3/Eigen/Geometry>
+#include </usr/include/eigen3/Eigen/Dense>
+
+using namespace Eigen;
 
 class ExtendedKalmanFilter {
 	public :
 		ExtendedKalmanFilter(double init_x, double init_y, double init_theta, double dt);
 		~ExtendedKalmanFilter();
 
-		vector<double> prior_state_estimate();
-		vector<double> prior_error_convariance_matrix();
-		vector<double> kalman_gain(vector<double> _P);
+		MatrixXd prior_state_estimate();
+	    MatrixXd prior_error_convariance_matrix();
+		MatrixXd kalman_gain(MatrixXd _P);
 		void jacobian_matrix(double v, double theta);
-		vector<double> state_estimate(vector<double> y, vector<double> x, vector<double> kalman_gain);
-		vector<double> posteriori_error_convariance_matrix(vector<double> kalman_gain, vector<double> _P);
-		vector<double> kalman_filter(double x, double y, double v);
+		MatrixXd state_estimate(MatrixXd y, MatrixXd x, MatrixXd kalman_gain);
+		MatrixXd posteriori_error_convariance_matrix(MatrixXd kalman_gain, MatrixXd _P);
+		MatrixXd kalman_filter(double x, double y, double v);
 		
 	private:
+        double dt;
+        Matrix3d A, B, P, Q;
+        MatrixXd x, u, C, R;
 };
