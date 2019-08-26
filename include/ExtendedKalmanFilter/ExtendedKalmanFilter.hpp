781a2c7787060da4a801b4b8d5ff16730e7b35ce
l1sum <l1sum@icloud.com>
2019-08-26 19:45:57 +0900

    done creation EKF

--- include/ExtendedKalmanFilter/ExtendedKalmanFilter.hpp
+++ include/ExtendedKalmanFilter/ExtendedKalmanFilter.hpp
@@ -1,26 +1,42 @@
+#define EIGEN_NO_DEBUG
+#define EIGEN_DONT_VECTORIZE
+#define EIGEN_DONT_PARALLELIZE
+#define EIGEN_MPL2_ONLY
+
 #include <cmath>
 
-#include </usr/include/eigen3/Eigen/Core>
-#include </usr/include/eigen3/Eigen/Geometry>
-#include </usr/include/eigen3/Eigen/Dense>
+#include <Eigen/Core>
+#include <Eigen/Geometry>
+#include <Eigen/Dense>
 
 using namespace Eigen;
 
 class ExtendedKalmanFilter {
-	public :
-		ExtendedKalmanFilter(double init_x, double init_y, double init_theta, double dt);
-		~ExtendedKalmanFilter();
+    public :
 
-		MatrixXd prior_state_estimate();
-	    MatrixXd prior_error_convariance_matrix();
-		MatrixXd kalman_gain(MatrixXd _P);
-		void jacobian_matrix(double v, double theta);
-		MatrixXd state_estimate(MatrixXd y, MatrixXd x, MatrixXd kalman_gain);
-		MatrixXd posteriori_error_convariance_matrix(MatrixXd kalman_gain, MatrixXd _P);
-		MatrixXd kalman_filter(double x, double y, double v);
-		
-	private:
+        ExtendedKalmanFilter(double init_x, double init_y, double init_theta, double dt);
+        ~ExtendedKalmanFilter();
+
+        EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+
+        Matrix<double, 3, 1> prior_state_estimate();
+        Matrix<double, 3, 3> prior_error_covariance_matrix();
+        Matrix<double, 3, 2> kalman_gain(Matrix<double, 3, 3> _P);
+        void jacobian_matrix(double v, double theta);
+        Matrix<double, 3, 1> state_estimate(Matrix<double, 2, 1> y, Matrix<double, 3, 1> x, Matrix<double, 3, 2> kalman_gain);
+        Matrix<double, 3, 3> posteriori_error_covariance_matrix(Matrix<double, 3, 2> kalman_gain, Matrix<double, 3, 3> _P);
+        Matrix<double, 2, 1> kalman_filter(double _x, double _y, double _v);
+
         double dt;
-        Matrix3d A, B, P, Q;
-        MatrixXd x, u, C, R;
+        Matrix<double, 3, 3> A;
+        Matrix<double, 3, 3> B;
+        Matrix<double, 3, 3> P;
+        Matrix<double, 3, 3> Q;
+
+        Matrix<double, 2, 2> R;
+
+        Matrix<double, 3, 1> x;
+        Matrix<double, 3, 1> u;
+        Matrix<double, 2, 3> C;
+
 };
--- src/ExtendedKalmanFilter.cpp
+++ src/ExtendedKalmanFilter.cpp
@@ -1,58 +1,102 @@
 #include "../include/ExtendedKalmanFilter/ExtendedKalmanFilter.hpp"
 
 #include <cmath>
+#include <Eigen/Core>
+#include <Eigen/Geometry>
+#include <Eigen/Dense>
+#include <Eigen/LU>
 
 using namespace std;
+using namespace Eigen;
 
 ExtendedKalmanFilter::ExtendedKalmanFilter(double init_x, double init_y, double init_theta, double dt){
     dt = dt;
 
-    x << init_x, init_y, init_theta;
+    x << init_x,
+         init_y,
+         init_theta;
 
-    u << dt*cos(init_theta), dt*sin(init_theta), dt;
+    u << dt*cos(init_theta),
+         dt*sin(init_theta),
+         dt;
 
-    A << 1, 0, 0,
-         0, 1, 0,
-         0, 0, 1;
+    A << 1.0, 0.0, 0.0,
+         0.0, 1.0, 0.0,
+         0.0, 0.0, 1.0;
 
-    B << 0.5, 0, 0,
-         0, 0.5, 0,
-         0, 0, 0.5;
+    B << 0.5, 0.0, 0.0,
+         0.0, 0.5, 0.0,
+         0.0, 0.0, 0.5;
 
-    C << 1, 0, 0,
-         0, 1, 0;
+    C << 1.0, 0.0, 0.0,
+         0.0, 1.0, 0.0;
 
-    P << 0.5, 0, 0,
-         0, 0.5, 0,
-         0, 0, 0.5;
+    P << 0.5, 0.0, 0.0,
+         0.0, 0.5, 0.0,
+         0.0, 0.0, 0.5;
 
-    Q << 10, 0, 0,
-         0, 10, 0,
-         0, 0, 10;
+    Q << 10.0, 0.0, 0.0,
+         0.0, 10.0, 0.0,
+         0.0, 0.0, 10.0;
 
-    R << 3, 0,
-         0, 3;
+    R << 3.0, 0.0,
+         0.0, 3.0;
 }
 
-ExtendedKalmanFilter::~ExtendedKalmanFilter(){}
 
-MatrixXd ExtendedKalmanFilter::prior_state_estimate(){
+ExtendedKalmanFilter::~ExtendedKalmanFilter(){
 }
 
-MatrixXd ExtendedKalmanFilter::prior_error_convariance_matrix(){
+
+Matrix<double, 3, 1> ExtendedKalmanFilter::prior_state_estimate(){
+    return A*x + B*u;
 }
 
-MatrixXd ExtendedKalmanFilter::kalman_gain(MatrixXd _P){
+
+Matrix<double, 3, 3> ExtendedKalmanFilter::prior_error_covariance_matrix(){
+    return A*P*A.transpose() + Q;
 }
+
+
+Matrix<double, 3, 2> ExtendedKalmanFilter::kalman_gain(Matrix<double, 3, 3> _P){
+    return _P*C.transpose()*((C*_P*C.transpose())+R).inverse();
+}
+
 
 void ExtendedKalmanFilter::jacobian_matrix(double v, double theta){
+    A << 1.0, 0.0, v*dt*sin(theta),
+         0.0, 1.0, -v*dt*cos(theta),
+         0.0, 0.0, 1.0;
 }
 
-MatrixXd ExtendedKalmanFilter::state_estimate(MatrixXd y, MatrixXd x, MatrixXd kalman_gain){
+
+Matrix<double, 3, 1> ExtendedKalmanFilter::state_estimate(Matrix<double, 2, 1> y, Matrix<double, 3, 1> x, Matrix<double, 3, 2> kalman_gain){
+    return x + kalman_gain*(y - C*x);
 }
 
-MatrixXd ExtendedKalmanFilter::posteriori_error_convariance_matrix(MatrixXd kalman_gain, MatrixXd _P){
+
+Matrix<double, 3, 3> ExtendedKalmanFilter::posteriori_error_covariance_matrix(Matrix<double, 3, 2> kalman_gain, Matrix<double, 3, 3> _P){
+    return (MatrixXd::Identity(3, 3) - kalman_gain*C)*P;
 }
 
-MatrixXd ExtendedKalmanFilter::kalman_filter(double x, double y, double v){
+Matrix<double, 2, 1> ExtendedKalmanFilter::kalman_filter(double _x, double _y, double _v){
+    Matrix<double, 3, 1> value_of_prior_state_estimate = prior_state_estimate();
+    jacobian_matrix(_v, value_of_prior_state_estimate(2));
+    Matrix<double, 3, 3> value_of_prior_error_covariance_matrix = prior_error_covariance_matrix();
+    Matrix<double, 3, 2> kalman_gain_matrix = kalman_gain(value_of_prior_error_covariance_matrix);
+
+    Matrix<double, 2, 1> observation_matrix;
+    observation_matrix << _x, _y;
+
+    Matrix<double, 3, 1> value_state_estimate = state_estimate(observation_matrix, value_of_prior_state_estimate, kalman_gain_matrix);
+    Matrix<double, 3, 3> value_posteriori_error_covariance_matrix = posteriori_error_covariance_matrix(kalman_gain_matrix, value_of_prior_error_covariance_matrix);
+    double est_x = value_state_estimate(0, 0);
+    double est_y = value_state_estimate(1, 0);
+
+    x = value_state_estimate;
+    P = value_posteriori_error_covariance_matrix;
+
+    Matrix<double, 2, 1> est;
+    est << est_x, est_y;
+    return est;
 }
