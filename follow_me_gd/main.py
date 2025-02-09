be500e779694a9a78aa757d340aab0d13b629608
Mitsuki1025 <menka123nachi16@icloud.com>
2025-02-09 17:34:35 +0900

    fix: some major errors

--- follow_me_gd/main.py
+++ follow_me_gd/main.py
@@ -7,8 +7,9 @@ from std_msgs.msg import String
 from sensor_msgs.msg import LaserScan
 from geometry_msgs.msg import Twist
 from nav_msgs.msg import Odometry
+from rclpy.qos import QoSProfile, QoSHistoryPolicy, QoSReliabilityPolicy
 
-from util import Point, PointData
+from follow_me.util import Point, PointData
 
 # Turtlebot3 Waffle Specs
 MAX_LINEAR  = 0.26 # m/s
@@ -19,8 +20,14 @@ class FollowMe(Node):
 
     def __init__(self):
         super().__init__("follow_me")
+        qos_profile = QoSProfile(
+            history=QoSHistoryPolicy.KEEP_LAST,
+            depth=10,
+            reliability=QoSReliabilityPolicy.BEST_EFFORT  # Make sure it's reliable
+        )
+
         self.pub_vel = self.create_publisher(Twist, "/cmd_vel", 10) 
-        self.sub_ydl = self.create_subscription(LaserScan, "/scan", self.ydlidar_callback, 10)
+        self.sub_ydl = self.create_subscription(LaserScan, "/scan", self.ydlidar_callback, qos_profile=qos_profile)
         self.sub_odm = self.create_subscription(Odometry, "/odometry", self.odometry_callback, 10)
         self.sub_cmd = self.create_subscription(String, "/follow_me/cmd", self.signal_callback, 10)
 
@@ -29,7 +36,7 @@ class FollowMe(Node):
         self.min_range:  float = 0.2    # min detection range [m]
         #######################
 
-        self.status:      bool = False
+        self.status:      bool = True
 
         self.sensor_x:          float = 0.0
         self.sensor_y:          float = 0.0
@@ -69,38 +76,47 @@ class FollowMe(Node):
                 data: PointData = PointData(
                     index = i,
                     point = ydlidar_points[i],
-                    existence_rate = self.calc_normal_distribution(i, 360, len(ydlidar_points)) * ydlidar_points[i].distance()
+                    existence_rate = self.calc_normal_distribution(i, 360, int(len(ydlidar_points)) * ydlidar_points[i].distance())
                 )
                 self.data_list.append(data)
         else:
             self.updatePlayerPoint(msg.angle_increment, ydlidar_points)
             for i in range(len(ydlidar_points)):
-                self.data_list[i].point = ydlidar_points[i]
-                self.data_list[i].existence_rate = self.cost(self.player_point, ydlidar_points[i]) + \
-                                                   self.data_list[i].existence_rate * \
-                                                   self.calc_normal_distribution(i, self.player_index, len(ydlidar_points))
+                try:
+                    self.data_list[i].point = ydlidar_points[i]
+                    self.data_list[i].existence_rate = self.cost(self.player_point, ydlidar_points[i]) + \
+                                                       self.data_list[i].existence_rate * \
+                                                       self.calc_normal_distribution(i, self.player_index, len(ydlidar_points))
+                except IndexError as e:
+                    break
 
         # normalization
         total:      float = 0.0
         max_val:    float = 0.0
         max_index:    int = 0
         for i in range(len(ydlidar_points)):
-            if (self.data_list[i].existence_rate == 0): self.data_list[i].existence_rate = 0.001
-            total += self.data_list[i].existence_rate
-            if (max_val < self.data_list[i].existence_rate):
-                max_val     = self.data_list[i].existence_rate
-                max_index   = i
+            try:
+                if (self.data_list[i].existence_rate == 0): self.data_list[i].existence_rate = 0.001
+                total += self.data_list[i].existence_rate
+                if (max_val < self.data_list[i].existence_rate):
+                    max_val     = self.data_list[i].existence_rate
+                    max_index   = i
+            except IndexError as e:
+                break
         for i in range(len(ydlidar_points)):
-            self.data_list[i].existence_rate /= total
+            try:
+                self.data_list[i].existence_rate /= total
+            except IndexError as e:
+                break
 
         # if enabled, publish twist according to estimated target's position
-        if (status):
+        if (self.status):
             self.player_index = max_index
             self.player_point = ydlidar_points[self.player_index]
             
             twist: Twist = Twist()
-            twist.linear.x  = self.calcStraight()
-            twist.angular.z = self.calcAngle() 
+            twist.linear.x  = self.calcStraight(self.player_point)
+            twist.angular.z = self.calcAngle(self.player_point) 
             self.pub_vel.publish(twist)
 
         new_position: Point = self.transform_absolute_to_relative(self.player_point)
@@ -110,7 +126,7 @@ class FollowMe(Node):
         self.last_absolute_position = new_position
 
         # if enabled, visualize lidar data
-        if (view_laser): self.view_ydlidar(ydlidar_points) 
+        if (self.view_laser): self.view_ydlidar(ydlidar_points) 
         return
 
 
@@ -141,7 +157,7 @@ class FollowMe(Node):
         index_distance: float = np.abs(target_index - center_index)
         if (index_distance > index_size / 2.0): index_distance -= index_size
         index_distance /= 95.0
-        normal_distribution: float = 1 / np.sqrt(2.0 * np.pi()) * np.exp((-index_distance * index_distance) / 2.0)
+        normal_distribution: float = 1.0 / np.sqrt(2.0 * np.pi) * np.exp((-index_distance * index_distance) / 2.0)
         return normal_distribution
 
 
@@ -174,12 +190,12 @@ class FollowMe(Node):
 
     @staticmethod
     def toDegree(rad: float) -> float:
-        return rad * 180 / np.pi()
+        return rad * 180 / np.pi
 
 
     @staticmethod
     def toRadian(angle: float) -> float:
-        return (angle * np.pi()) / 180
+        return (angle * np.pi) / 180
 
 
     @staticmethod
@@ -191,7 +207,7 @@ class FollowMe(Node):
 
     @staticmethod
     def calcStraight(target_point: Point) -> float:
-        result: float = 0
+        result: float = 0.0
         if (target_point.y < 0 and np.abs(target_point.y) > 70):
             result = np.abs(target_point.y) * 0.001875
         if (np.abs(result) > MAX_LINEAR): result = MAX_LINEAR
@@ -200,8 +216,8 @@ class FollowMe(Node):
 
     def updatePlayerPoint(self, angle_increment: float, ydlidar_points: list[Point]) -> None:
         relative_theta: float = self.toRadian(self.last_degree - self.sensor_degree)
-        if (np.abs(relative_theta) > np.pi()): 
-            relative_theta = (2 * np.pi() - relative_theta)
+        if (np.abs(relative_theta) > np.pi): 
+            relative_theta = (2 * np.pi - relative_theta)
 
         self.player_index += int(relative_theta / angle_increment)
         self.last_degree = self.sensor_degree
@@ -210,10 +226,10 @@ class FollowMe(Node):
 
     def view_ydlidar(self, points: list[Point]) -> None:
         """optional"""
-        img: np.ndarray = np.zero((1000, 1000, 3), dtype=np.uint8) 
+        img: np.ndarray = np.zeros((1000, 1000, 3), dtype=np.uint8) 
         for point in points:
-            x = point.x + 500
-            y = point.y + 250
+            x = int(point.x) + 500
+            y = int(point.y) + 250
             cv2.circle(img, center=(x, y), radius=1, color=(0, 255, 0), thickness=1)
         cv2.circle(img, center=(self.player_point.x + 500, self.player_point.y + 250), radius=5, color=(0, 0, 255), thickness=1)
         cv2.imshow("follow_me", img)
