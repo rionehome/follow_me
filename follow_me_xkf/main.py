3000bea4f82825d5d66b6d11a90a641931edef99
Mitsuki1025 <menka123nachi16@icloud.com>
2025-02-09 18:19:24 +0900

    fix: some major errors

--- follow_me_xkf/main.py
+++ follow_me_xkf/main.py
@@ -7,9 +7,10 @@ from std_msgs.msg import String
 from sensor_msgs.msg import LaserScan
 from geometry_msgs.msg import Twist
 from nav_msgs.msg import Odometry
+from rclpy.qos import QoSProfile, QoSHistoryPolicy, QoSReliabilityPolicy
 
-from util import Point
-from xkf import ExtendedKalmanFilter
+from follow_me_xkf.util import Point
+from follow_me_xkf.xkf import ExtendedKalmanFilter
 
 # Turtlebot3 Waffle Specs
 MAX_LINEAR  = 0.26 # m/s
@@ -20,8 +21,14 @@ class FollowMe(Node):
 
     def __init__(self):
         super().__init__("follow_me")
+        qos_profile = QoSProfile(
+            history=QoSHistoryPolicy.KEEP_LAST,
+            depth=10,
+            reliability=QoSReliabilityPolicy.BEST_EFFORT  # Make sure it's reliable
+        )
+
         self.pub_vel = self.create_publisher(Twist, "/cmd_vel", 10) 
-        self.sub_ydl = self.create_subscription(LaserScan, "/scan", self.ydlidar_callback, 10)
+        self.sub_ydl = self.create_subscription(LaserScan, "/scan", self.ydlidar_callback, qos_profile=qos_profile)
         self.sub_cmd = self.create_subscription(String, "/follow_me/cmd", self.signal_callback, 10)
 
         ## User configurable ##
@@ -29,12 +36,12 @@ class FollowMe(Node):
         self.min_range:  float = 0.2    # min detection range [m]
         #######################
 
-        self.status:                     bool = False
+        self.status:                     bool = True
         self.player_point:              Point = Point(0, 0)
         self.ydlidar_points:      list[Point] = []
         self.xkf: ExtendedKalmanFilter        = None
 
-        self.get_logger().info(f"[+][follow_me] Successfully initialized.")
+        self.get_logger().info("Successfully initialized.")
 
 
     def ydlidar_callback(self, msg) -> None:
@@ -53,7 +60,7 @@ class FollowMe(Node):
         min_distance: float = sys.float_info.max 
         min_index:      int = 0
         for i in range(len(self.ydlidar_points)):
-            distance: float = np.sqrt(np.pow(self.ydlidar_points[i].x - self.player_point.x, 2) + np.pow(self.ydlidar_points[i].y - self.player_point.y, 2));
+            distance: float = np.sqrt((self.ydlidar_points[i].x - self.player_point.x) ** 2 + (self.ydlidar_points[i].y - self.player_point.y) ** 2)
             if (distance < min_distance):
                 min_distance = distance
                 min_index = i
@@ -68,7 +75,7 @@ class FollowMe(Node):
             self.player_point = self.xkf.kalman_filter(self.ydlidar_points[min_index].x, self.ydlidar_points[min_index].y, dx, dy)
 
         # if enabled, publish twist according to estimated target's position
-        if (status):
+        if (self.status):
             twist: Twist = Twist()
             twist.linear.x  = self.calcStraight()
             twist.angular.z = self.calcAngle() 
@@ -82,15 +89,15 @@ class FollowMe(Node):
     def signal_callback(self, msg) -> None:
         if (msg.data == "start" and self.status == False): self.status = True
         elif (msg.data == "stop" and self.status == True): self.status = False
-        else: self.get_logger().info(f"[-][follow_me] Bad signal received: {msg.data}")
+        else: self.get_logger().info(f"Bad signal received: {msg.data}")
 
-        if (self.status): self.get_logger().info(f"[+][follow_me] Activated!")
+        if (self.status): self.get_logger().info(f"Activated!")
         else:
             twist: Twist = Twist()
             twist.linear.x  = 0
             twist.angular.z = 0
             self.pub_vel.publish(twist)
-            self.get_logger().info(f"[+][follow_me] Deactivated!")
+            self.get_logger().info(f"Deactivated!")
         return
 
 
@@ -101,7 +108,7 @@ class FollowMe(Node):
 
 
     def calcStraight(self) -> float:
-        result = 0
+        result = 0.0
         if (self.player_point.y < 0 and np.abs(self.player_point.y) > 70):
             result = np.abs(self.player_point.y) * 0.001875
         if (np.abs(result) > MAX_LINEAR): result = MAX_LINEAR
@@ -110,10 +117,10 @@ class FollowMe(Node):
 
     def view_ydlidar(self) -> None:
         """optional"""
-        img: np.ndarray = np.zero((1000, 1000, 3), dtype=np.uint8) 
+        img: np.ndarray = np.zeros((1000, 1000, 3), dtype=np.uint8) 
         for point in self.ydlidar_points:
-            x = point.x + 500
-            y = point.y + 250
+            x = int(point.x) + 500
+            y = int(point.y) + 250
             cv2.circle(img, center=(x, y), radius=1, color=(0, 255, 0), thickness=1)
         cv2.circle(img, center=(self.player_point.x + 500, self.player_point.y + 250), radius=5, color=(0, 0, 255), thickness=1)
         cv2.imshow("follow_me", img)
